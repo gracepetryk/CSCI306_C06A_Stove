6397dca9ea1cd49298c3bf96cecb92f58113279e
Grace Petryk <gracepetryk99@gmail.com>
2020-01-23 11:50:07 -0700

    actually added setting enum

--- src/Setting.java
+++ src/Setting.java
@@ -1,4 +1,20 @@
 
 public enum Setting {
+	OFF ("---"),
+	LOW ("--+"),
+	MEDIUM ("-++"),
+	HIGH ("+++");
+	
+	private final String setting;
+	
+	Setting(String settingString) {
+		this.setting = settingString;
+	}
+	
+	@Override
+	public String toString() {
+		return setting;
+	}
+	
 
 }
